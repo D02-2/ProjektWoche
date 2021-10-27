2a843c730b0eb92a24924d2912417b9335e47b34
Ghania-Aitdriss <aitdgha77@hotmail.fr>
2021-10-27 09:41:02 +0200

    keine Änderung

--- index.js
+++ index.js
@@ -94,9 +94,96 @@ if (category === 'B') {
         console.log(quizBookArray.join(''));
     }
 
+
+
     if (resultArr.join('') === quizBookWithSpace.join('')) {
         console.log('you are the winner');
-    } else console.log('game over')
+    } else {
+        console.clear()
+        console.log('GAME OVER!!!!!')
+        
+        const gameRepeat = prompt(`   möchtest du nochmal spielen  (J / N))? `);
+        if(gameRepeat=== 'J'){
+            const category = prompt(` Welche Kategorie möchtest du spielen (Bücher oder Filme (B / F))? `);   ;
+        }
+        if (category === 'B') {
+    const quizBook = books[Math.round(Math.random() * 9)];
+
+    const quizBookArray = Array.from(quizBook)
+    const selectTitle = () => {
+
+        const result = quizBookArray.map((letter) => {
+            if (letter !== ' ') {
+                return letter.replace(letter, ' _ ')
+            } else {
+                return ' - '
+            }
+        });
+        return result
+    }
+    console.clear();
+    console.log(selectTitle().join(''));
+    let resultArr = selectTitle();
+    let emojiResultArr = [];
+    const quizBookWithSpace = quizBook.replaceAll(' ', '-').split('').map(char => ` ${char} `);
+    
+    while (emojiResultArr.length < 10 && resultArr.join('') !== quizBookWithSpace.join('')) {
+        const guessLetter = prompt('Bitte gib einen Buchstaben ein ');
+
+        function searchLetter(arr, char) {
+            let rightChar = false;
+            const result = arr.map(letter => {
+                if (char.toLowerCase() === letter.toLowerCase()) {
+                    rightChar = true;
+                    return ` ${letter} `
+                } else if (letter !== ' ') {
+                    return ' _ '
+                } else return '-'
+
+            })
+            if (!rightChar) {
+
+                emojiResultArr.push(emojiArr[emojiResultArr.length]);
+
+            }
+            console.log(emojiResultArr.join(''));
+           
+            for (let i = 0; i < result.length; i++) {
+                if (resultArr[i] !== result[i] && resultArr[i] === ' _ ') {
+                    resultArr[i] = result[i]
+
+                }
+
+            }
+
+            return resultArr;
+        }
+        console.clear();
+        console.log(searchLetter(quizBookArray, guessLetter).join(''));
+        console.log(quizBookArray.join(''));
+    }
+
+
+
+    if (resultArr.join('') === quizBookWithSpace.join('')) {
+        console.log('you are the winner');
+    } else {
+        console.clear()
+        console.log('GAME OVER!!!!!')
+        
+        const gameRepeat = prompt(`   möchtest du nochmal spielen  (J / N))? `);
+        if(gameRepeat=== 'J'){
+            const category = prompt(` Welche Kategorie möchtest du spielen (Bücher oder Filme (B / F))? `);   ;
+        }
+
+        }
+        
+    
+}
+
+
+        }
+        
     
 }
 
